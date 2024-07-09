3364110041c042cbe428022a4d71fcb32cf758c3
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-07-09 19:39:53 +0100

    Initial implementation of RedisCacheService

---
+++ Services/RedisCacheService.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace HospitalMiddleware.Services
+{
+    public class RedisCacheService
+    {
+        private readonly IConnectionMultiplexer _redis;
+
+        public RedisCacheService(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        public async Task SetAsync(string key, string value, TimeSpan expiration)
+        {
+            var db = _redis.GetDatabase();
+            await db.StringSetAsync(key, value, expiration);
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            var db = _redis.GetDatabase();
+            return await db.StringGetAsync(key);
+        }
+    }
+}
