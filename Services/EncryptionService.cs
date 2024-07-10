e23cd243b5f1ac89a776cc022415df9f649cd878
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-07-10 16:29:40 +0100

    Initial implementation of EncryptionService

---
+++ Services/EncryptionService.cs
@@ -0,0 +1,46 @@
+﻿using HospitalMiddleware.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HospitalMiddleware.Services
+{
+    public class EncryptionService : IEncryptionService
+    {
+        private readonly byte[] _key;
+        private readonly byte[] _iv;
+
+        public EncryptionService()
+        {
+            _key = Encoding.UTF8.GetBytes("your_secret_key_here");
+            _iv = Encoding.UTF8.GetBytes("your_iv_here");
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = _key;
+            aesAlg.IV = _iv;
+
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            using var msEncrypt = new MemoryStream();
+            using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using var swEncrypt = new StreamWriter(csEncrypt);
+            swEncrypt.Write(plainText);
+            return Convert.ToBase64String(msEncrypt.ToArray());
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            var cipherBytes = Convert.FromBase64String(cipherText);
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = _key;
+            aesAlg.IV = _iv;
+
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            using var msDecrypt = new MemoryStream(cipherBytes);
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            return srDecrypt.ReadToEnd();
+        }
+    }
+}
