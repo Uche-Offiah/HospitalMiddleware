a87796191f6c8e144f0dcbda3e8951258e81e3f7
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-07-11 18:16:54 +0100

    Bug fix for Encrypt method in EncryptionService

--- Services/EncryptionService.cs
+++ Services/EncryptionService.cs
@@ -15,19 +15,48 @@ namespace HospitalMiddleware.Services
             _iv = Encoding.UTF8.GetBytes("EncryptionIV1234");
         }
 
+
+        //Yet to figure out why this Encrypt method does not work
+        //public string Encrypt(string plainText)
+        //{
+        //    using var aesAlg = Aes.Create();
+        //    aesAlg.Key = _key;
+        //    aesAlg.IV = _iv;
+
+        //    var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+        //    using var msEncrypt = new MemoryStream();
+        //    using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+        //    using var swEncrypt = new StreamWriter(csEncrypt);
+        //    swEncrypt.Write(plainText);
+        //    return Convert.ToBase64String(msEncrypt.ToArray());
+        //}
+
         public string Encrypt(string plainText)
         {
-            using var aesAlg = Aes.Create();
-            aesAlg.Key = _key;
-            aesAlg.IV = _iv;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = _key;
+                aesAlg.IV = _iv;
 
-            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-            using var msEncrypt = new MemoryStream();
-            using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-            using var swEncrypt = new StreamWriter(csEncrypt);
-            swEncrypt.Write(plainText);
-            return Convert.ToBase64String(msEncrypt.ToArray());
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                            
+                        }
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                       
+                }
+               
+            }
+                
         }
+
 
         public string Decrypt(string cipherText)
         {
